ccc62dd830d598bfe1269283ca0891556c27dfe5
osoroka2019@itcollege.lviv.ua <osoroka2019@itcollege.lviv.ua>
2022-10-13 10:08:23 +0300

    x

--- 5__lab/tests.py
+++ 5__lab/tests.py
@@ -1,4 +1,11 @@
 import unittest
 from lab import Rocket
 
-class TestMylab(unittest.TestCase)
+class TestMylab(unittest.TestCase):
+    def test_converter_correct(self):
+        mass = 3700000
+        for mass in[3700000,3800000]:
+            obj = Rocket("Ares V", mass)
+            self.assertAlmostEqual(obj.convert_mass(), mass * 2.20462262)
+
+    def test_mock    
